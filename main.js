0cdfc1570d0cc69462e2f9dc6f487014c921aa3b
Ana Luiza Albano <145017189+Aninha0928@users.noreply.github.com>
2025-03-28 08:05:18 -0300

    .

--- main.js
+++ main.js
@@ -15,10 +15,10 @@ for (let i = 0; i < botoes.length; i++) {
 }
 
 const contadores = document.querySelectorAll(".contador");
-const tempoObjetivo1 = new Date("2025-12-01T08:35:248");
-const tempoObjetivo2 = new Date("2023-12-05T00:00:00");
-const tempoObjetivo3 = new Date("2023-12-30T00:00:00");
-const tempoObjetivo4 = new Date("2024-02-01T00:00:00");
+const tempoObjetivo1 = new Date("2025-12-01T00:00:00");
+const tempoObjetivo2 = new Date("2025-10-01T00:00:00");
+const tempoObjetivo3 = new Date("2025-12-19T00:00:00");
+const tempoObjetivo4 = new Date("2025-12-21T00:00:00");
 
 const tempos = [tempoObjetivo1, tempoObjetivo2, tempoObjetivo3, tempoObjetivo4];
 
@@ -35,7 +35,7 @@ function calculaTempo(tempoObjetivo) {
     minutos %= 60;
     horas %= 24;
     if (tempoFinal > 0) {
-        return [dias, horas, minutos, segundos];
+        return [meses, semanas, dias, horas];
     } else {
         return [0, 0, 0, 0];
     }
